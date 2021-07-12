dc8f22ca4e64bbd8a8ef4a04407f69eb2537a6c6
miverboven <michiel.verboven@thomasmore.be>
2021-07-13 01:37:29 +0200

    Form update

--- src/main/java/fact/it/springgithubactionsdemo/controller/IndexController.java
+++ src/main/java/fact/it/springgithubactionsdemo/controller/IndexController.java
@@ -4,17 +4,29 @@ import fact.it.springgithubactionsdemo.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
-
-        Person john = new Person("John","Doe");
-        model.addAttribute("person",john);
-
+    public String index() {
         return "index";
+    }
+
+    @PostMapping("/output")
+    public String output(HttpServletRequest request, Model model) {
+
+        String givenName = request.getParameter("fname");
+        String surname = request.getParameter("lname");
+
+        Person person = new Person(givenName,surname);
+        model.addAttribute("person",person);
+
+        return "output";
+
     }
 
 }
